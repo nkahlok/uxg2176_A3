555cae7db5aa0421c77a0d93bcf2b97e4bff05c3
CHIA-QI-MING-JEREMY <tas7700123@gmail.com>
2025-11-23 23:32:32 +0800

    drone puzzle + keypad completed

--- Assets/UXG2176 A3/Scripts/Interactables/KeypadPanel.cs
+++ Assets/UXG2176 A3/Scripts/Interactables/KeypadPanel.cs
@@ -1,10 +1,20 @@
 public class KeypadPanel : Interactable
 {
+    GameInput gameInput;
     Keypad keypad;
 
     private void Start()
     {
+        gameInput = Player.Instance.GetComponent<GameInput>();
+        gameInput.OnKeypadReturnAction += GameInput_OnKeypadReturnAction;
+
         keypad = transform.GetChild(0).GetComponent<Keypad>();
+    }
+
+    private void GameInput_OnKeypadReturnAction(object sender, System.EventArgs e)
+    {
+        keypad.ClearInput();
+        Player.Instance.SwitchMode(Player.PlayerState.PLAYER);
     }
 
     public override void Activate()
